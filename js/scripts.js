5094d0dbd9b584fecca1fa7e242f6d0bb2c79c3b
carias08 <ca9488@gmail.com>
2021-03-22 01:30:13 -0400

    Finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 let body = document.body;
 let container = document.getElementById("container");
+let colorSite = document.getElementById("color-site");
+body.appendChild(colorSite);
 
 const createPixels = () => {
     // 4800
@@ -19,13 +21,17 @@ let pixels = document.getElementsByClassName("small-divs");
 const createColorSquares = () => {
     let colorsArray = ["pink", "red", "tomato", "orange", "yellow", "MediumSeaGreen", "DodgerBlue", "Violet", "black", "white"];
 
+    let colorSquaresContainer = document.createElement("div");
+    colorSquaresContainer.setAttribute("id", "color-squares-container");
+    colorSite.appendChild(colorSquaresContainer);
+
     let chooseColorText = document.createElement("p");
-    chooseColorText.innerText = "Choose Your Color: "
-    body.appendChild(chooseColorText)
+    chooseColorText.innerText = "Choose Your Color"
+    colorSquaresContainer.appendChild(chooseColorText)
 
     let colorContainer = document.createElement("div");
     colorContainer.setAttribute("id", "color-container")
-    body.appendChild(colorContainer)
+    colorSquaresContainer.appendChild(colorContainer)
 
     for(let i = 0; i < colorsArray.length; i++){
         let index = i + 1;
@@ -43,16 +49,43 @@ createColorSquares();
 let myColors = document.querySelectorAll(".my-colors");
 let chosenColor = "black"; //default color
 
+// display chosen color 
+
+const createDisplayChosenColor = (color) => {
+    // current color text 
+    let colorDisplayContainer = document.createElement("div");
+    colorDisplayContainer.setAttribute("id", "color-display-container");
+    colorSite.appendChild(colorDisplayContainer);
+
+    let currentColorText = document.createElement("p");
+    currentColorText.innerText = "Current Color"
+    colorDisplayContainer.appendChild(currentColorText)
+
+
+    let newDisplay = document.createElement('div');
+    newDisplay.setAttribute("id", "display-color");
+    newDisplay.style.backgroundColor = " " + color;
+    colorDisplayContainer.appendChild(newDisplay)
+}
+
+createDisplayChosenColor(chosenColor)
+
+let myDisplayedColor = document.getElementById("display-color");
+
+// choose color 
+
 const chooseColor = () => {
     myColors.forEach(color => {
         color.addEventListener('click', function(){
             chosenColor = color.id;
+            myDisplayedColor.style.backgroundColor = chosenColor;
         })
     })
 }
 chooseColor()
 
 const draw = () => {
+
     for(let i = 0; i < pixels.length; i++){
         pixels[i].addEventListener("mouseover", function(evt){
             if (evt.buttons === 1){
@@ -65,6 +98,7 @@ const draw = () => {
 }
 
 container.onmousedown = draw;
+
 
 // clear button 
 
